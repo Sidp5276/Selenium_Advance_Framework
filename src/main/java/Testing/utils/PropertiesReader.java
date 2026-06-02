11fa6c2c82edb28f878bf91ebe7eec46e4259dea
Siddharth <sidp5276@gmail.com>
2026-06-02 13:23:40 +0530

    Property reader util

--- src/main/java/Testing/utils/PropertiesReader.java
+++ src/main/java/Testing/utils/PropertiesReader.java
@@ -10,9 +10,9 @@ public class PropertiesReader {
     public static String readKey(String key) {
         Properties p;
         String user_dir = System.getProperty("user.dir");
-        // //Users/promode/Documents/TTA/ATB15xSeleniumAdvanceFramework
+
         String file_path = user_dir + "/src/main/resources/data.properties";
-        // -> /Users/promode/Documents/TTA/ATB15xSeleniumAdvanceFramework/src/main/resources/data.properties
+
         try {
             FileInputStream fileInputStream = new FileInputStream(file_path);
             p = new Properties();
