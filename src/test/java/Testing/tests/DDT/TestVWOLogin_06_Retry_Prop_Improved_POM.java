a54ff14186af1f77a7e6f4ad9892b4a51cd63522
Siddharth <sidp5276@gmail.com>
2026-06-05 21:49:31 +0530

    Test VWO login retry prom improved POM

--- src/test/java/Testing/tests/DDT/TestVWOLogin_06_Retry_Prop_Improved_POM.java
+++ src/test/java/Testing/tests/DDT/TestVWOLogin_06_Retry_Prop_Improved_POM.java
@@ -6,8 +6,6 @@ import org.testng.annotations.Test;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
-import org.testng.annotations.Test;
-
 import static Testing.driver.DriverManager.getDriver;
 
 @Test(retryAnalyzer = RetryAnalyzer.class )
